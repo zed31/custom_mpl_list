f4cb66b8651bdb115a92903d174579d895cd0a34
Clement Francois <clement.francois@epitech.eu>
2017-08-18 16:21:23 +0200

    mpl_list : Add unitary tests

---
+++ include/mpl_push_back.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace mpl_custom {
+	template<class container, class T>
+	struct push_back_impl;
+
+	template<template<class...> class container, class... T, class insert>
+	struct push_back_impl<container<T...>, insert> {
+		using type = container<T..., insert>;
+	};
+
+	template<class container, class insert>
+	using push_back = typename push_back_impl<container, insert>::type;
+}
--- src/mpl.cpp
+++ src/mpl.cpp
@@ -4,25 +4,15 @@
 #include "../include/mpl_list.hpp"
 #include "../include/mpl_size.hpp"
 #include "../include/mpl_push_front.hpp"
+#include "../include/mpl_push_back.hpp"
 
 int main() {
-	static_assert( 
-		std::is_same<
-			mpl_custom::list<int, char, float>, 
-			mpl_custom::change<
-				std::tuple<int, char, float>, 
-				mpl_custom::list
-			>
-		>::value, 
-		"Error : expected mpl_custom::list<int, char, float>"
-	);
 	static_assert(
 		mpl_custom::size<std::tuple<int, float, char, void>>::value == 4, ""
 	);
-	static_assert(
-		std::is_same<
-			mpl_custom::push_front<std::tuple<int, float>, char>,
-			std::tuple<char, int,float>
-		>::value, "Error, std::tuple<char, int, float> expected"
-	);
+
+	/* New way of testing things Because I worth it !!!! */
+	mpl_custom::list<int, float, double>{} = mpl_custom::push_back<mpl_custom::list<int, float>, double>{};
+	mpl_custom::list<int, char, float>{} = mpl_custom::change<std::tuple<int, char, float>, mpl_custom::list>{};
+	std::tuple<char, int, float>{} = mpl_custom::push_front<std::tuple<int, float>, char>{};
 }
